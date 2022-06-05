806d53311c0ad95701a921acfee661370c5c5c61
Muhamad Rino Permana <mhmdrino@gmail.com>
2022-06-05 08:06:00 +0700

    Delete travelino directory

--- travelino/assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-var mainListDiv = document.getElementById("mainListDiv"),
-  mediaButton = document.getElementById("mediaButton");
-
-mediaButton.onclick = function () {
-  "use strict";
-
-  mainListDiv.classList.toggle("show_list");
-  mediaButton.classList.toggle("active");
-};
